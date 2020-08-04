2c99e7926ea005c0a5f5024e313385ad8d99a185
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2020-08-04 17:38:55 +1000

    Update help message

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const loudfoot = require("../configs/footer.json");
 const config = require("../configs/config.json");
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message) => {
     let check = message.channel.id;
     if(message.channel.type == "dm") return message.channel.send("Sorry can't do that here!");
     if(check == config.configs.chID) {
@@ -11,7 +11,9 @@ module.exports.run = async (bot, message, args) => {
         .setThumbnail(URL = "https://loudmining.com/media/lm-us.png")
         .addField("<help", "This help message")
         .addField("<pools", "List of LOUD Mining Pools and their locations")
-        .addField("<network ticker", "Network stats of Pool coins")
+        .addField("<network (ticker)", "Network stats of Pool coins *eg <network upx*")
+        .addField("<mystats (pool location) (address)", "Get statistics of your miner from specific pool *eg <mystats usa UPX1oKXYsdsHvwcGeeDyyjECUERFv4d1cgGZXEgMuCnE1QJUN66p8cgaKQxtMvdKtYJtF41cdnQLP7W5YBeApXQE6oTQQRDUXg*")
+        .addField("<stats (network) (ticker)", "Get specific pool statistics per ticker *eg <stats USA upx*")
         .addField("<software", "Software links per network")
         .addField("Powered by © LOUD Mining 2018-2020", loudfoot.footer);
         return message.channel.send(helpEmbed);
