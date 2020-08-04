ee5c7d8c10b7e05bfe127e1b6eb916a9e2ef0633
LOUD Mining <39448763+LOUD-Mining@users.noreply.github.com>
2020-08-04 05:56:11 -0500

    fixing my fuckup

--- loudbot.js
+++ loudbot.js
@@ -1,51 +1,60 @@
 const config = require("./configs/config.json")
 const tokenFile = require("./configs/token.json")
-const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client();
+const Discord = require("discord.js")
+const fs = require("fs")
+const bot = new Discord.Client()
 
-function catchErr(err) {
-	bot.users.get(config.configs.adminUser).send(config.configs.errMsg);
-	bot.users.get(config.configs.adminUser).send("Error contained below: \n```" + err + "```");
-}
+// create a new Discord Collection for the list of command files
+bot.commands = new Discord.Collection()
 
-bot.commands = new Discord.Collection();
+// populate that collection with commands from files
 fs.readdir("./commands/", (err, files) => {
-	if(err) console.log(err);
+
+	if(err) console.log(err)
 	let jsfile = files.filter(f => f.split(".").pop() == "js")
 	if(jsfile.length <= 0) {
-		console.log("Couldn't find any valid commands!");
-		return;
+		console.log("Couldn't find any valid commands!")
+		return
 	}
+
 	jsfile.forEach((f,i) => {
-		let props = require(`./commands/${f}`);
-		console.log(`${f} loaded!`);
-		bot.commands.set(props.help.name, props);
-	});
-});
+		let props = require(`./commands/${f}`)
+		console.log(`${f} loaded!`)
+		bot.commands.set(props.help.name, props)
+	})
+
+})
+
+// when bot is fully initialized we announce that the bot is fully operational
 try {
 bot.on("ready", function() {
-	console.log(`Awww Shit! Here come's that asshole ${bot.user.tag}`);
-	bot.user.setStatus("LOUDest pool on the BLOCK!");
+	console.log(`Awww Shit! Here come's that asshole ${bot.user.tag}`)
+	bot.user.setActivity("with people's emotions")
 });
 }
 catch {
-	catchErr(err, message);
+	catchErr(err, message)
 }
+
+// bot looks at messages in server and determines if it is a command or not and then runs said command
 try {
 bot.on("message", async message => {
-	if(message.author.bot) return;
-	if(message.author.bot.channelType == "dm") return;
-	prefix = config.configs.prefix;
-	messageArray = message.content.split(" ");
-	cmd = messageArray[0];
-	args = messageArray.slice(1);
-	if (message.content.charAt(0) != prefix) return;
-	commandfile = bot.commands.get(cmd.slice(prefix.length));
-	if(commandfile) commandfile.run(bot,message,args);
-});
-bot.login(tokenFile.token);
+	
+	if(message.author.bot) return
+	if(message.author.bot.channelType == "dm") return
+	
+	prefix = config.configs.prefix
+	messageArray = message.content.split(" ")
+	cmd = messageArray[0]
+	args = messageArray.slice(1)
+	
+	commandfile = bot.commands.get(cmd.slice(prefix.length))
+	if(commandfile) commandfile.run(bot,message,args)
+})
+
+// logs the bot into discord, make sure to edit the token.json with your own discord oauth token!
+bot.login(token.discord)
 }
 catch{
-	catchErr(err);
+	catchErr(err)
 }
